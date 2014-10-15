b56fb4b1cd8028cbcd7015811ad074f67fbcdcbb
tyarkoni <tyarkoni@gmail.com>
2014-10-14 22:03:50 -0500

    tweaked setup for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from distutils.core import setup
+from transitions.version import __version__
 
 if len(set(('test', 'easy_install')).intersection(sys.argv)) > 0:
     import setuptools
@@ -14,19 +15,17 @@ if 'setuptools' in sys.modules:
             test='nose>=0.10.1')
     )
 
-# fetch version from within module
-with open(os.path.join('transitions', 'version.py')) as f:
-    exec(f.read())
-
-setup(name="transitions",
-      version=__version__,
-      description="A lightweight, object-oriented Python state machine implementation.",
-      maintainer='Tal Yarkoni',
-      maintainer_email='tyarkoni@gmail.com',
-      url='http://github.com/tyarkoni/transitions',
-      packages=["transitions"],
-      package_data={'transitions': ['data/*'],
-                    'transitions.tests': ['data/*']
-                    },
-      **extra_setuptools_args
-      )
+setup(
+    name="transitions",
+    version=__version__,
+    description="A lightweight, object-oriented Python state machine implementation.",
+    author='Tal Yarkoni',
+    author_email='tyarkoni@gmail.com',
+    url='http://github.com/tyarkoni/transitions',
+    packages=["transitions"],
+    package_data={'transitions': ['data/*'],
+                'transitions.tests': ['data/*']
+                },
+    download_url='https://github.com/tyarkoni/transitions/archive/%s.tar.gz' % __version__,
+    **extra_setuptools_args
+    )
