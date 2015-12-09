2622d4d44cad2bf85c43263ca662fb31138f15c1
Sander van de Graaf <mail@svdgraaf.nl>
2015-12-09 13:18:29 +0100

    make the diagram class optional

--- transitions/core.py
+++ transitions/core.py
@@ -464,8 +464,8 @@ class Machine(object):
         else:
             func(*event_data.args, **event_data.kwargs)
 
-    def get_graph(self, title=None):
-        return AGraph(self).get_graph(title)
+    def get_graph(self, title=None, diagram_class=AGraph):
+        return diagram_class(self).get_graph(title)
 
     def __getattr__(self, name):
         if name.startswith('__'):
