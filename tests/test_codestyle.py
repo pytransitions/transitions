7884c5891e00b2696b7596a6d586f6627a08631c
Alexander Neumann <aleneum@gmail.com>
2018-10-25 17:31:10 +0200

    fix minor style issues; pylint: ignore invalid escape sequences;

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -5,7 +5,7 @@ import pycodestyle
 class TestCodeFormat(unittest.TestCase):
     def test_conformance(self):
         """Test that we conform to PEP-8."""
-        style = pycodestyle.StyleGuide(quiet=False, ignore=['E501'])
+        style = pycodestyle.StyleGuide(quiet=False, ignore=['E501', 'W605'])
         style.input_dir('transitions')
         # style.input_dir('tests')
         result = style.check_files()
