4c1122861c1abb18351814905016921f02aaaaaa
Alexander Neumann <aleneum@gmail.com>
2018-12-06 23:16:56 +0100

    fix issues for async and testing
    * fix condition check
    * exclude test_async in python2 and default python3 (could be < 3.5)
    * allow synchronized functions in Machine.callback

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,19 +1,24 @@
-from unittest import TestCase
 import asyncio
 from unittest.mock import MagicMock
 
 from transitions.extensions.asyncio import AsyncMachine
+from .test_core import TestTransitions
 
-async def await_false():
-    await asyncio.sleep(0.1)
-    return False
-async def await_true():
-    await asyncio.sleep(0.1)
-    return True
 
-class TestAsync(TestCase):
+class TestAsync(TestTransitions):
+
+    @staticmethod
+    async def await_false():
+        await asyncio.sleep(0.1)
+        return False
+
+    @staticmethod
+    async def await_true():
+        await asyncio.sleep(0.1)
+        return True
 
     def setUp(self):
+        super(TestAsync, self).setUp()
         self.machine = AsyncMachine(states=['A', 'B', 'C'], transitions=[['go', 'A', 'B']], initial='A')
 
     def test_async_machine_cb(self):
@@ -29,13 +34,13 @@ class TestAsync(TestCase):
         self.assertEqual(m.state, 'B')
         self.assertTrue(mock.called)
 
-    def test_async_condition_cb(self):
+    def test_async_condition(self):
         m = self.machine
-        m.add_transition('proceed', 'A', 'C',  conditions=await_true, unless=await_false)
+        m.add_transition('proceed', 'A', 'C', conditions=self.await_true, unless=self.await_false)
         m.proceed()
         self.assertEqual(m.state, 'C')
 
-    def test_sync_conditions_cb(self):
+    def test_sync_conditions(self):
         mock = MagicMock()
 
         def sync_process():
