65e69393bf8c975a12516a77e449ba37b28f49db
Alexander Neumann <aleneum@gmail.com>
2019-08-16 13:09:34 +0200

    fix test_factory attribute lookup

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -17,24 +17,24 @@ class TestFactory(TestCase):
         self.assertFalse(hasattr(machine_cls, 'set_edge_state'))
 
         graph_cls = self.factory.get_predefined(graph=True)
-        self.assertTrue(hasattr(graph_cls, 'set_edge_state'))
+        self.assertTrue(hasattr(graph_cls, '_get_graph'))
         nested_cls = self.factory.get_predefined(nested=True)
-        self.assertFalse(hasattr(nested_cls, 'set_edge_state'))
+        self.assertFalse(hasattr(nested_cls, '_get_graph'))
         self.assertTrue(hasattr(nested_cls, '_traverse'))
 
         locked_cls = self.factory.get_predefined(locked=True)
-        self.assertFalse(hasattr(locked_cls, 'set_edge_state'))
+        self.assertFalse(hasattr(locked_cls, '_get_graph'))
         self.assertFalse(hasattr(locked_cls, '_traverse'))
         self.assertTrue('__getattribute__' in locked_cls.__dict__)
 
         locked_nested_cls = self.factory.get_predefined(nested=True, locked=True)
-        self.assertFalse(hasattr(locked_nested_cls, 'set_edge_state'))
+        self.assertFalse(hasattr(locked_nested_cls, '_get_graph'))
         self.assertTrue(hasattr(locked_nested_cls, '_traverse'))
         self.assertEqual(locked_nested_cls.__getattribute__, locked_cls.__getattribute__)
         self.assertNotEqual(machine_cls.__getattribute__, locked_cls.__getattribute__)
 
         graph_locked_cls = self.factory.get_predefined(graph=True, locked=True)
-        self.assertTrue(hasattr(graph_locked_cls, 'set_edge_state'))
+        self.assertTrue(hasattr(graph_locked_cls, '_get_graph'))
         self.assertEqual(graph_locked_cls.__getattribute__, locked_cls.__getattribute__)
 
         graph_nested_cls = self.factory.get_predefined(graph=True, nested=True)
