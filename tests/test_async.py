740b154dfdd265e9c9045e32a95ad10e37577684
Alexander Neumann <aleneum@gmail.com>
2019-08-27 11:25:41 +0200

    asyncmachine rewrite

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,10 +1,16 @@
-import asyncio
-from unittest.mock import MagicMock
-from transitions.extensions.asyncio import AsyncMachine
+try:
+    from transitions.extensions.asyncio import AsyncMachine
+    import asyncio
+except (ImportError, SyntaxError):
+    asyncio = None
 
+
+from unittest.mock import MagicMock
+from unittest import skipIf
 from .test_core import TestTransitions
 
 
+@skipIf(asyncio is None, "AsyncMachine requires asyncio and contextvars suppport")
 class TestAsync(TestTransitions):
 
     @staticmethod
@@ -16,6 +22,11 @@ class TestAsync(TestTransitions):
     async def await_true():
         await asyncio.sleep(0.1)
         return True
+
+    @staticmethod
+    async def await_never_return():
+        await asyncio.sleep(100)
+        return None
 
     @staticmethod
     def synced_true():
@@ -34,14 +45,14 @@ class TestAsync(TestTransitions):
 
         m = self.machine
         m.after_state_change = async_process
-        m.go()
+        asyncio.run(m.go())
         self.assertEqual(m.state, 'B')
         self.assertTrue(mock.called)
 
     def test_async_condition(self):
         m = self.machine
         m.add_transition('proceed', 'A', 'C', conditions=self.await_true, unless=self.await_false)
-        m.proceed()
+        asyncio.run(m.proceed())
         self.assertEqual(m.state, 'C')
 
     def test_async_enter_exit(self):
@@ -59,7 +70,7 @@ class TestAsync(TestTransitions):
         m = self.machine
         m.on_exit_A(async_exit)
         m.on_enter_B(async_enter)
-        m.go()
+        asyncio.run(m.go())
         self.assertTrue(exit_mock.called)
         self.assertTrue(enter_mock.called)
 
@@ -71,6 +82,25 @@ class TestAsync(TestTransitions):
 
         m = self.machine
         m.add_transition('proceed', 'A', 'C', conditions=self.synced_true, after=sync_process)
-        m.proceed()
+        asyncio.run(m.proceed())
         self.assertEqual(m.state, 'C')
         self.assertTrue(mock.called)
+
+    def test_multiple_models(self):
+        async def fix():
+            if not m2.is_B():
+                await m2.fix()
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        m1 = AsyncMachine(states=['A', 'B', 'C'], initial='A')
+        m2 = AsyncMachine(states=['A', 'B', 'C'], initial='A')
+        m2.add_transition(trigger='go', source='A', dest='B', conditions=self.await_never_return)
+        m2.add_transition(trigger='fix', source='A', dest='C', conditions=self.await_true)
+        m1.add_transition(trigger='go', source='A', dest='B', conditions=self.await_true, after='go')
+        m1.add_transition(trigger='go', source='B', dest='C', after=fix)
+        loop.run_until_complete(asyncio.gather(m1.go()))
+
+        assert m1.is_C()
+        assert m2.is_C()
