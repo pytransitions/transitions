04d73557913ad73e625be13fed2882612216095d
Alexander Neumann <aleneum@gmail.com>
2020-01-10 15:24:05 +0100

    check versions instead of import attempt in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
+import sys
 import os.path
 
 collect_ignore = []
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-try:
-    import asyncio
-    import contextvars
-except ImportError:
+
+if sys.version_info.major < 3 or sys.version_info.major == 3 and sys.version_info.minor < 7:
     collect_ignore.append(os.path.join(current_dir, "test_async.py"))
