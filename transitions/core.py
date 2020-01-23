53f1ec42a5034337351d381ed34b6232e61027e2
Jonathan Senecal <contact@jonathansenecal.com>
2020-01-23 16:38:34 -0500

    Use properties in conditions

--- transitions/core.py
+++ transitions/core.py
@@ -1038,15 +1038,24 @@ class Machine(object):
 
     @staticmethod
     def resolve_callable(func, event_data):
-        """ Converts a model's method name or a path to a callable into a callable.
+        """ Converts a model's property name, method name or a path to a callable into a callable.
             If func is not a string it will be returned unaltered.
         Args:
-            func (str or callable): Method name or a path to a callable
+            func (str or callable): Property name, method name or a path to a callable
             event_data (EventData): Currently processed event
         Returns:
             callable function resolved from string or func
         """
         if isinstance(func, string_types):
+            try:
+                if isinstance(getattr(type(event_data.model), func), property):
+                    # `func` is a property, generate a callable from it
+                    def predicate():
+                        return func
+                    return predicate
+            except AttributeError:
+                pass
+
             try:
                 func = getattr(event_data.model, func)
             except AttributeError:
