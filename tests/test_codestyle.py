0f1392773da12528b647778707891718e25256c2
Alexander Neumann <aleneum@gmail.com>
2022-08-25 14:07:23 +0200

    add mypy to test; conditional install mypy in gha

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -2,6 +2,11 @@ import unittest
 import pycodestyle
 from os.path import exists
 
+try:
+    import mypy
+except ImportError:
+    mypy = None
+
 
 class TestCodeFormat(unittest.TestCase):
     def test_conformance(self):
@@ -16,3 +21,12 @@ class TestCodeFormat(unittest.TestCase):
         result = style.check_files()
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    @unittest.skipIf(mypy is None, "mypy not found")
+    def test_mypy(self):
+        import subprocess
+        call = ['mypy', '--config-file', 'mypy.ini', 'transitions']
+
+        # when run from root directory (e.g. tox) else when run from test directory (e.g. pycharm)
+        project_root = '.' if exists('transitions') else '..'
+        subprocess.check_call(call, cwd=project_root)
