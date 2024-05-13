cb913e099f8c3d3cff9b837451477f7d72d1b32a
Alexander Neumann <aleneum@gmail.com>
2024-05-13 18:23:08 +0200

    improve typing information in pyi files

--- transitions/extensions/asyncio.pyi
+++ transitions/extensions/asyncio.pyi
@@ -1,6 +1,6 @@
-from ..core import Condition, Event, EventData, Machine, State, Transition, StateConfig, ModelParameter, TransitionConfig
+from ..core import Callback, Condition, Event, EventData, Machine, State, Transition, StateConfig, ModelParameter, TransitionConfig
 from .nesting import HierarchicalMachine, NestedEvent, NestedState, NestedTransition
-from typing import Any, Optional, List, Type, Dict, Deque, Callable, Union, Iterable, DefaultDict, Literal, Sequence
+from typing import Any, Awaitable, Optional, List, Type, Dict, Deque, Callable, Union, Iterable, DefaultDict, Literal, Sequence
 from asyncio import Task
 from functools import partial
 from logging import Logger
@@ -10,6 +10,9 @@ from contextvars import ContextVar
 from ..core import StateIdentifier, CallbacksArg, CallbackList
 
 _LOGGER: Logger
+
+AsyncCallbackFunc = Callable[..., Awaitable[Optional[bool]]]
+AsyncCallback = Union[str, AsyncCallbackFunc]
 
 class AsyncState(State):
     async def enter(self, event_data: AsyncEventData) -> None: ...  # type: ignore[override]
@@ -42,7 +45,7 @@ class AsyncEvent(Event):
     machine: AsyncMachine
     transitions: DefaultDict[str, List[AsyncTransition]]  # type: ignore
 
-    async def trigger(self, model: object, *args: List, **kwargs: Dict[str, Any]) -> bool: ...  # type: ignore[override]
+    async def trigger(self, model: object, *args: List[Any], **kwargs: Dict[str, Any]) -> bool: ...  # type: ignore[override]
     async def _trigger(self, event_data: AsyncEventData) -> bool: ...  # type: ignore[override]
     async def _process(self, event_data: AsyncEventData) -> bool: ...  # type: ignore[override]
 
@@ -56,12 +59,12 @@ class AsyncMachine(Machine):
     state_cls: Type[NestedAsyncState]
     transition_cls: Type[AsyncTransition]
     event_cls: Type[AsyncEvent]
-    async_tasks: Dict[int, List[Task]]
+    async_tasks: Dict[int, List[Task[Any]]]
     events: Dict[str, AsyncEvent]  # type: ignore
     queued: Union[bool, Literal["model"]]
-    protected_tasks: List[Task]
-    current_context: ContextVar
-    _transition_queue_dict: Dict[int, Deque[Callable]]
+    protected_tasks: List[Task[Any]]
+    current_context: ContextVar[Optional[Task[Any]]]
+    _transition_queue_dict: Dict[int, Deque[AsyncCallbackFunc]]
     def __init__(self, model: Optional[ModelParameter] = ...,
                  states: Optional[Union[Sequence[StateConfig], Type[Enum]]] = ...,
                  initial: Optional[StateIdentifier] = ...,
@@ -75,17 +78,16 @@ class AsyncMachine(Machine):
                  **kwargs: Dict[str, Any]) -> None: ...
     def add_model(self, model: Union[Union[Literal["self"], object], Sequence[Union[Literal["self"], object]]],
                   initial: Optional[StateIdentifier] = ...) -> None: ...
-    async def dispatch(self, trigger: str, *args: List, **kwargs: Dict[str, Any]) -> bool: ...  # type: ignore[override]
-    async def callbacks(self, funcs: Iterable[Union[str, Callable]], event_data: AsyncEventData) -> None: ...  # type: ignore[override]
-    async def callback(self, func: Union[str, Callable], event_data: AsyncEventData) -> None: ...  # type: ignore[override]
+    async def dispatch(self, trigger: str, *args: List[Any], **kwargs: Dict[str, Any]) -> bool: ...  # type: ignore[override]
+    async def callbacks(self, funcs: Iterable[Callback], event_data: AsyncEventData) -> None: ...  # type: ignore[override]
+    async def callback(self, func: AsyncCallback, event_data: AsyncEventData) -> None: ...  # type: ignore[override]
     @staticmethod
-    async def await_all(callables: List[Callable]) -> List: ...
+    async def await_all(callables: List[AsyncCallbackFunc]) -> Awaitable[List[Any]]: ...
     async def switch_model_context(self, model: object) -> None: ...
     def get_state(self, state: Union[str, Enum]) -> AsyncState: ...
-    async def process_context(self, func: partial, model: object) -> bool: ...
+    async def process_context(self, func: Callable[[], Awaitable[None]], model: object) -> bool: ...
     def remove_model(self, model: object) -> None: ...
-    def _process(self, trigger: partial) -> bool: ...
-    async def _process_async(self, trigger: partial, model: object) -> bool: ...
+    async def _process_async(self, trigger: Callable[[], Awaitable[None]], model: object) -> bool: ...
 
 
 class HierarchicalAsyncMachine(HierarchicalMachine, AsyncMachine):  # type: ignore
@@ -93,7 +95,7 @@ class HierarchicalAsyncMachine(HierarchicalMachine, AsyncMachine):  # type: igno
     transition_cls: Type[NestedAsyncTransition]
     event_cls: Type[NestedAsyncEvent]  # type: ignore
     async def trigger_event(self, model: object, trigger: str, # type: ignore[override]
-                            *args: List, **kwargs: Dict[str, Any]) -> bool: ...
+                            *args: List[Any], **kwargs: Dict[str, Any]) -> bool: ...
     async def _trigger_event(self, event_data: AsyncEventData, trigger: str) -> bool: ...  # type: ignore[override]
 
 
@@ -101,18 +103,18 @@ class AsyncTimeout(AsyncState):
     dynamic_methods: List[str]
     timeout: float
     _on_timeout: CallbacksArg
-    runner: Dict[int, Task]
-    def __init__(self, *args: List, **kwargs: Dict[str, Any]) -> None: ...
+    runner: Dict[int, Task[Any]]
+    def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]) -> None: ...
     async def enter(self, event_data: AsyncEventData) -> None: ...  # type: ignore[override]
     async def exit(self, event_data: AsyncEventData) -> None: ...  # type: ignore[override]
-    def create_timer(self, event_data: AsyncEventData) -> Task: ...
+    def create_timer(self, event_data: AsyncEventData) -> Task[Any]: ...
     async def _process_timeout(self, event_data: AsyncEventData) -> None: ...
     @property
     def on_timeout(self) -> CallbackList: ...
     @on_timeout.setter
     def on_timeout(self, value: CallbacksArg) -> None: ...
 
-class _DictionaryMock(dict):
+class _DictionaryMock(Dict[Any, Any]):
     _value: Any
     def __init__(self, item: Any) -> None: ...
     def __setitem__(self, key: Any, item: Any) -> None: ...
