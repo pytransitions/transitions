864c6efe54761ab6bf030bcd40b25349e9fa3c10
Alexander Neumann <aleneum@gmail.com>
2024-06-18 13:48:36 +0200

    feat(experimental): remove 'expect' override
    Scope of expect_override was too narrow and does not solve the typing issue adequately
    this reverts #676

--- transitions/experimental/decoration.py
+++
@@ -1,3 +0,0 @@
-def expect_override(func):
-    setattr(func, "expect_override", True)
-    return func
--- transitions/experimental/decoration.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Callable, ParamSpec
-
-P = ParamSpec("P")
-
-
-def expect_override(func: Callable[P, bool | None]) -> Callable[P, bool | None]: ...
