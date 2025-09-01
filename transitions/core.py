5ed865c56f6ad23db0d5bd77e5d99f8215ab303c
Matthias Urlichs <matthias@urlichs.de>
2025-09-01 12:28:10 +0200

    Move Condition helper class to be subordinate of Transition
    as the reason for moving it up to module level no longer applies
    
    This reverts 892a59c.

--- transitions/core.py
+++ transitions/core.py
@@ -133,50 +133,6 @@ class State(object):
         return "<%s('%s')@%s>" % (type(self).__name__, self.name, id(self))
 
 
-class Condition(object):
-    """A helper class to call condition checks in the intended way.
-
-    Attributes:
-        func (str or callable): The function to call for the condition check
-        target (bool): Indicates the target state--i.e., when True,
-                the condition-checking callback should return True to pass,
-                and when False, the callback should return False to pass.
-    """
-
-    def __init__(self, func, target=True):
-        """
-        Args:
-            func (str or callable): Name of the condition-checking callable
-            target (bool): Indicates the target state--i.e., when True,
-                the condition-checking callback should return True to pass,
-                and when False, the callback should return False to pass.
-        Notes:
-            This class should not be initialized or called from outside a
-            Transition instance, and exists at module level (rather than
-            nesting under the transition class) only because of a bug in
-            dill that prevents serialization under Python 2.7.
-        """
-        self.func = func
-        self.target = target
-
-    def check(self, event_data):
-        """Check whether the condition passes.
-        Args:
-            event_data (EventData): An EventData instance to pass to the
-                condition (if event sending is enabled) or to extract arguments
-                from (if event sending is disabled). Also contains the data
-                model attached to the current machine which is used to invoke
-                the condition.
-        """
-        predicate = event_data.machine.resolve_callable(self.func, event_data)
-        if event_data.machine.send_event:
-            return predicate(event_data) == self.target
-        return predicate(*event_data.args, **event_data.kwargs) == self.target
-
-    def __repr__(self):
-        return "<%s(%s)@%s>" % (type(self).__name__, self.func, id(self))
-
-
 class Transition(object):
     """Representation of a transition managed by a ``Machine`` instance.
 
@@ -192,8 +148,47 @@ class Transition(object):
             but only if condition checks have been successful.
     """
 
+    class Condition(object):
+        """A helper class to call condition checks in the intended way.
+
+        Attributes:
+            func (str or callable): The function to call for the condition check
+            target (bool): Indicates the target state--i.e., when True,
+                    the condition-checking callback should return True to pass,
+                    and when False, the callback should return False to pass.
+        """
+
+        def __init__(self, func, target=True):
+            """
+            Args:
+                func (str or callable): Name of the condition-checking callable
+                target (bool): Indicates the target state--i.e., when True,
+                    the condition-checking callback should return True to pass,
+                    and when False, the callback should return False to pass.
+            """
+            self.func = func
+            self.target = target
+
+        def check(self, event_data):
+            """Check whether the condition passes.
+            Args:
+                event_data (EventData): An EventData instance to pass to the
+                    condition (if event sending is enabled) or to extract arguments
+                    from (if event sending is disabled). Also contains the data
+                    model attached to the current machine which is used to invoke
+                    the condition.
+            """
+            predicate = event_data.machine.resolve_callable(self.func, event_data)
+            if event_data.machine.send_event:
+                return predicate(event_data) == self.target
+            return predicate(*event_data.args, **event_data.kwargs) == self.target
+
+        def __repr__(self):
+            return "<%s(%s)@%s>" % (type(self).__name__, self.func, id(self))
+
     dynamic_methods = ['before', 'after', 'prepare']
     """ A list of dynamic methods which can be resolved by a ``Machine`` instance for convenience functions. """
+
     condition_cls = Condition
     """ The class used to wrap condition checks. Can be replaced to alter condition resolution behaviour
         (e.g. OR instead of AND for 'conditions' or AND instead of OR for 'unless') """
